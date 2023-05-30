b061a6349b4de4807bbba0e184cac869ba222b35
joaopaulocordeiro <joaopaulocordeiro21@gmail.com>
2023-05-30 17:29:32 -0300

    feature: criacao de estilização global

---
+++ src/styles/theme/defaultTheme.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  colors: {
+    "text-primary": "#202020",
+    "text-secunday": "#FFFFFF",
+    "background-color": "#FBFBFB",
+    "gray-board": "#CCCCCC",
+    "gradient-primary":
+      "linear-gradient(270deg, rgba(216,53,197,1) 0%, rgba(161,31,241,1) 100%);",
+  },
+  textSizes: {
+    "title-title-l": "1.625rem",
+    "title-subtitle-l": "1.5rem",
+    "text-text-l": "1.125rem",
+    "text-text-m": "1rem",
+    "text-text-s": "0.625rem",
+  },
+} as const;
